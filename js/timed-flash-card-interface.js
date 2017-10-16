f2b7745fcad30fc8e00379e94263996f63b1526a
Elrey Belmonti and Linda Luu <linda@>
2017-10-16 13:15:45 -0700

    add buttons with values for correct and incorrect answers

--- js/timed-flash-card-interface.js
+++ js/timed-flash-card-interface.js
@@ -1,7 +1,11 @@
 import { FlashCard } from './../js/timed-flash-card.js';
 
 $(document).ready(function(){
-  $("input[name='answer']").prop('value', FlashCard.cards()[FlashCard.questionKeys()[0]]);
+  $("button[name='answer']").prop('value', FlashCard.cards()[FlashCard.questionKeys()[0]]);
+    $("button[name='wrong1']").prop('value', "wrong1");
+      $("button[name='wrong2']").prop('value', "wrong2");
   $("label#question1").text(FlashCard.questionKeys()[0]);
+    $("span.wrong1").text(FlashCard.questionKeys()[0]);
+    $("span.wrong2").text(FlashCard.questionKeys()[0]);
   $("span.answer").text(FlashCard.cards()[FlashCard.questionKeys()[0]]);
 });
