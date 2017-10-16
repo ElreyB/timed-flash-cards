d25bfc769777c931b6ae5a4cc3f94ededbf19537
Elrey Belmonti and Linda Luu <linda@>
2017-10-16 11:31:58 -0700

    add ability to add question to form

--- js/timed-flash-card-interface.js
+++ js/timed-flash-card-interface.js
@@ -2,5 +2,6 @@ import { FlashCard } from './../js/timed-flash-card.js';
 
 $(document).ready(function(){
   $("input[name='answer']").prop('value', FlashCard.cards()[FlashCard.questionKeys()[0]]);
+  $("label#question1").text(FlashCard.questionKeys()[0]);
   $("span.answer").text(FlashCard.cards()[FlashCard.questionKeys()[0]]);
 });
