ac757505d584f487aeb914aa57b462def67d6af2
Elrey Belmonti <elbelmonti@gmail.com>
2017-10-17 23:44:41 -0700

    design font, button, timer and points size

--- js/timed-flash-card.js
+++ js/timed-flash-card.js
@@ -31,25 +31,25 @@ export class FlashCard {
     let forms;
     const answers = [
       `<div class="col-md-6">
-        <button type="button" name="answer" index="${index}" value="${card.answer}" class="btn button btn-answer">
+        <button type="button" name="answer" index="${index}" value="${card.answer}" class="btn btn-lg button btn-answer">
           <span class="answer">${card.answer}</span>
         </button>
         </div>`,
       `<div class="col-md-6">
         <button type="button" name="wrong1" index="${index}" value="${card
-        .wrongAnswers[0]}" class="btn button">
+        .wrongAnswers[0]}" class="btn btn-lg button">
           <span class="wrong1">${card.wrongAnswers[0]}</span>
         </button>
       </div>`,
       `<div class="col-md-6">
         <button type="button" name="wrong2" index="${index}" value="${card
-        .wrongAnswers[1]}" class="btn button">
+        .wrongAnswers[1]}" class="btn btn-lg button">
           <span class="wrong2">${card.wrongAnswers[1]}</span>
         </button>
       </div>`,
       `<div class="col-md-6">
         <button type="button" name="wrong2" index="${index}" value="${card
-        .wrongAnswers[2]}" class="btn button">
+        .wrongAnswers[2]}" class="btn btn-lg button">
           <span class="wrong2">${card.wrongAnswers[2]}</span>
         </button>
       </div>`
@@ -58,8 +58,19 @@ export class FlashCard {
     forms = `<form class="flash-cards card-${index} hide">
         <div class="panel">
           <div class="panel-heading panel-color">
-            <label for="question" id="question1">${card.question}</label>
-            <div class="timer"> </div> <span class="times-up hide">Times UP ⏰</span>
+            <div class="row">
+              <div class="col-md-2">
+                <div class="timer"> </div> <span class="times-up hide">Times UP ⏰</span>
+              </div>
+              <div class="col-md-8">
+                <div class="text-center">
+                  <label for="question" id="question1">${card.question}</label>
+                </div>
+              </div>
+              <div class="col-md-2">
+                <p class="points-title">Points: <span class="points"></span></p>
+              </div>
+            </div>
           </div>
           <div class="panel-body">
             <div class="row">
@@ -67,8 +78,9 @@ export class FlashCard {
             </div>
           </div>
           <div class="panel-footer">
-            <button type="button" name="button" class= "btn next">Next</button>
-            <div class="points"></div>
+            <div class="text-center">
+              <button type="button" name="button" class= "btn btn-lg next">Next</button>
+            </div>
           </div>
         </div>
       </form><br>`;
